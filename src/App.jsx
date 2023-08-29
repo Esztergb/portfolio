4a1feb4e12b71a9c70651a2dfd6c6985bc89a066
Eszter Barbuscia <gubiscom@yahoo.com>
2023-08-29 10:08:46 -0700

    progressbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,30 @@ import ProfilePic from './Components/ProfilePic'
 import Introduction from './Components/Introduction'
 import About from './Components/About'
 import Services from './Components/Services'
+import ProgressBar from './Components/ProgressBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <Header/>
-     <SideBar/>
-     <div className='ml-[90px] grid grid-cols-1 md:grid-cols-3'>
-        <div className='col-span-2 p-5'>
-          <Introduction/> 
-          <About/>  
-          <Services/>
+    <div className="w-screen">
+      <Header />
+      <SideBar />
+      <div
+        className="ml-[90px]  
+      grid grid-cols-1 md:grid-cols-3
+      "
+      >
+        <div className="col-span-2 p-5 mt-[90px]">
+          <Introduction />
+          <About />
+          <Services />
         </div>
-        <div className='hidden md:block'>
-          <ProfilePic/>
+        <div className="hidden md:block">
+          <ProgressBar />
+          <ProfilePic />
         </div>
-     </div>
-     
-    </>
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
